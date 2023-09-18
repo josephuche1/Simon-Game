46c760b63c70f631e753b37bfc9ad2cad88c6ef6
PARSIVALJ <uchejesusboy@gmail.com>
2023-09-19 00:26:12 +0300

    Added a fntion to check the users answer

--- game.js
+++ game.js
@@ -1,20 +1,33 @@
 let userClickedPattern = [];
 let gamePattern = [];
 let buttonColors = ["red", "blue",  "green", "yellow"];
+let level = 0;
 
 $(".btn").click(function(){
     let userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
     playSound(userChosenColor);
     animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length - 1);
 });
 
+$(document).keypress(function(){
+    nextSequence();
+    
+})
+
 function nextSequence(){
+    userClickedPattern = [];
     let randomNumber = Math.floor(Math.random()*4);
     let randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
     $("#"+randomChosenColor).fadeOut(100).fadeIn(100);
     playSound(randomChosenColor)
+    
+    
+    $("h1").text("Level "+level);
+    level++;
+    
 }
 
 function playSound(name){
@@ -29,8 +42,20 @@ function animatePress(currentColor){
     }, 100);
 }
 
+function checkAnswer(currentLevel){
+  if(gamePattern[currentLevel] == userClickedPattern[currentLevel]){
+    console.log("success");
+  }
+  else{
+    console.log("wrong");
+  }
 
-
-
-
-
+  if(gamePattern.length == userClickedPattern.length){
+    setTimeout(function () {
+        nextSequence();
+    }, 1000);
+    
+  }
+  
+  
+}
