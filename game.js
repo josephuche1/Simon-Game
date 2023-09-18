a930013091da4e2dbcc193b8825a0c2b7a3fb864
PARSIVALJ <uchejesusboy@gmail.com>
2023-09-18 18:46:16 +0300

    edited code to play sound upon clicking the buttons

--- game.js
+++ game.js
@@ -1,20 +1,28 @@
 let userClickedPattern = [];
 let gamePattern = [];
 let buttonColors = ["red", "blue",  "green", "yellow"];
-let randomChosenColor = buttonColors[nextSequence()];
 
-gamePattern.push(randomChosenColor);
+
+
 function nextSequence(){
     let randomNumber = Math.floor(Math.random()*4);
-    return randomNumber;
+    let randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+    $("#"+randomChosenColor).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColor)
 }
 
-$("#"+randomChosenColor).fadeOut(100).fadeIn(100);
-let audio = new Audio("./sounds/"+randomChosenColor+".mp3");
-audio.play();
-
-$("button").click(function(){
+$(".btn").click(function(){
     let userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
 });
+
+function playSound(name){
+    let audio = new Audio("./sounds/"+name+".mp3");
+    audio.play();
+}
+
+
+
 
